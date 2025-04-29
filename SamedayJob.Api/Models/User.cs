c1abe47d59a7ffe396aaeafe5580e0a9084251aa
Thobani Mjiyakho <thobani.mjiyakho@kcpivot.co.za>
2025-04-29 11:57:14 +0200

    Add Models

---
+++ SamedayJob.Api/Models/User.cs
@@ -0,0 +1,10 @@
+namespace SamedayJob.Api.Models;
+
+public class User{
+    public int Id {get; set;}
+    public string Email {get; set;}
+    public string PasswordHash { get; set; }
+    public string PhoneNumber { get; set; }
+    public bool IsPhoneNumberVerified { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
