2c77aaca5f9a028a9802acbe1185195ad5f5606a
Thobani <mjiyakhothobani@gmail.com>
2025-05-19 23:09:10 +0200

    Updated Dtos

--- SamedayJob.Api/DTOs/LoginDto.cs
+++ SamedayJob.Api/DTOs/LoginDto.cs
@@ -2,6 +2,6 @@ namespace SamedayJob.Api.DTOs;
 
 public class LoginDto
 {
-    public string Email {get; set;}
-    public string Password {get; set;}
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
--- SamedayJob.Api/DTOs/RegisterDto.cs
+++ SamedayJob.Api/DTOs/RegisterDto.cs
@@ -2,8 +2,8 @@ namespace SamedayJob.Api.DTOs;
 
 public class RegisterDto
 {
-    public string Name {get; set;}
-    public string PhoneNumber {get; set;}
-    public string Email {get; set;}
-    public string Password {get; set;}
+    public string Name { get; set; } = string.Empty;
+    public string PhoneNumber {get; set;} = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
