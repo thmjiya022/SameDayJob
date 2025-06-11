515f6eb9d510bdc28de354c003f6115e4af6ff43
Mjiyakho <mjiyakhothobani@gmail.com>
2025-06-11 10:23:09 +0200

    format Job model

--- samedayjob-frontend/src/models/Job.ts
+++ samedayjob-frontend/src/models/Job.ts
@@ -1,21 +1,21 @@
 export interface Job {
-  jobID: number;
-  title: string;
-  description: string;
-  budget: number;
-  location: string;
-  status: string;
-  postedAt: string;
-  postedBy: number;
-  categoryID: number;
-  category?: {
-    categoryID: number;
-    name: string;
-  };
-  client?: {
-    userID: number;
-    name: string;
-    email: string;
-    phoneNumber: string;
-  };
+	jobID: number;
+	title: string;
+	description: string;
+	budget: number;
+	location: string;
+	status: string;
+	postedAt: string;
+	postedBy: number;
+	categoryID: number;
+	category?: {
+		categoryID: number;
+		name: string;
+	};
+	client?: {
+		userID: number;
+		name: string;
+		email: string;
+		phoneNumber: string;
+	};
 }
